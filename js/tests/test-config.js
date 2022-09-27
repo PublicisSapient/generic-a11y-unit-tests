428d765b9e5956804600e5b36cb76a1f1c6430cf
Zoltan Hawryluk <zoltan.dulac@gmail.com>
2022-09-27 14:50:37 -0400

    Adding test files.

---
+++ js/tests/test-config.js
@@ -0,0 +1,20 @@
+"use strict";
+
+
+const { argv } = process;
+const filenameWithoutPath = argv[argv.length - 1];
+
+
+jest.setTimeout(10000);
+
+const config = {
+  "BASE_URL": `http://localhost:8888`,
+  "KEYPRESS_TIMEOUT": 250,
+  "KEYPRESS_FAST_TIMEOUT": 100,
+  "DESKTOP_WIDTH": 1024,
+  "DESKTOP_HEIGHT": 768,
+  "MOBILE_WIDTH": 400,
+  "MOBILE_HEIGHT": 800
+};
+
+export default config; 
